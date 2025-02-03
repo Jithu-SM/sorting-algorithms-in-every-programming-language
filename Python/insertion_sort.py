cc687074bdd8c3d853db2ded989f75f53c949a89
Jithu S <82302841+Jithu-SM@users.noreply.github.com>
2025-02-03 21:16:07 +0530

    Update insertion_sort.py

--- Python/insertion_sort.py
+++ Python/insertion_sort.py
@@ -7,3 +7,20 @@ def insertion_sort(arr):
             j -= 1
         arr[j + 1] = key
     return arr
+
+def bucket_sort(arr):
+    max_val = max(arr)
+    size = max_val // len(arr)
+    buckets = [[] for _ in range(len(arr))]
+    for num in arr:
+        index = num // size
+        if index != len(arr):
+            buckets[index].append(num)
+        else:
+            buckets[len(arr) - 1].append(num)
+    for bucket in buckets:
+        bucket=insertion_sort(bucket)
+    result = []
+    for bucket in buckets:
+        result.extend(bucket)
+    return result
