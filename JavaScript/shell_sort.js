19eefbef32fe0f0ff06cfd1ad324b9c0d544b3fb
Jithu S <82302841+Jithu-SM@users.noreply.github.com>
2025-03-11 09:58:21 +0530

    shell sort

---
+++ JavaScript/shell_sort.js
@@ -0,0 +1,18 @@
+function shellSort(arr) {
+    let n = arr.length;
+    for (let gap = Math.floor(n / 2); gap > 0; gap = Math.floor(gap / 2)) {
+        for (let i = gap; i < n; i++) {
+            let temp = arr[i];
+            let j;
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                arr[j] = arr[j - gap];
+            }
+            arr[j] = temp;
+        }
+    }
+    return arr;
+}
+
+let array = [12, 34, 54, 2, 3, 0];
+array = shellSort(array);
+console.log("Sorted array:", array);
